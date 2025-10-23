d6800521b4c79d6154d07fea9a2498384411f890
Babatunde Jelugbo <babatunde.jelugbo@telus.com>
2025-10-23 12:08:18 -0400

    moving json to route base

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(cors());
 app.use(logAuditTrails);
 app.use('/webhook', webhookRoutes);
-app.use(express.json());
 
 // server.js
 
